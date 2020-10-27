b6dd4bfcce92a9d68146378541797f216d0e06bd
Abdullah Alsharrah <abdullahalsharrah@Abdullahs-Air.hotspot>
2020-10-27 16:55:40 +0300

    solved bareed tasks

--- bareed.js
+++ bareed.js
@@ -43,11 +43,17 @@ class Point {
  **********************************************************/
 class Wallet {
   // implement Wallet!
-  constructor(money = 0) {}
+  constructor(money = 0) {
+    this.money = money;
+  }
 
-  credit = amount => {};
+  credit = amount => { 
+    this.money += amount;
+  };
 
-  debit = amount => {};
+  debit = amount => {
+    this.money -= amount;
+  };
 }
 
 /**********************************************************
@@ -63,6 +69,13 @@ class Wallet {
  **********************************************************/
 class Person {
   // implement Person!
+  constructor(name, x, y){
+  this.name = name
+  this.location = new Point(x, y)
+}
+  wallet = new Wallet(0)
+  moveTo = (point)=> this.location = point;
+
 }
 
 /**********************************************************
@@ -80,8 +93,18 @@ class Person {
  *
  * new vendor = new Vendor(name, x, y);
  **********************************************************/
-class Vendor {
+class Vendor extends Person{
   // implement Vendor!
+   range = 5
+   price = 1
+
+  sellTo = (customer, numberOfIceCreams) => {
+    this.moveTo(customer.location);
+
+    customer.wallet.debit(numberOfIceCreams * this.price);
+
+    this.wallet.credit(numberOfIceCreams * this.price);
+  }
 }
 
 /**********************************************************
@@ -100,8 +123,26 @@ class Vendor {
  *
  * new customer = new Customer(name, x, y);
  **********************************************************/
-class Customer {
+class Customer extends Person {
   // implement Customer!
+  constructor(name, x, y){
+    super(name, x, y)
+    this.wallet.money = 10;
+  }
+
+  _isInRange = (vendor) => (vendor.range >= vendor.location.distanceTo(this.location));
+
+  _haveEnoughMoney = (vendor, numberOfIceCreams) => (this.wallet.money / vendor.price) >= numberOfIceCreams;
+
+
+  requestIceCream = (vendor, numberOfIceCreams) => {
+    if ((this._isInRange(vendor) && this._haveEnoughMoney(vendor, numberOfIceCreams)) === true){
+      vendor.sellTo(this, numberOfIceCreams);
+
+    }
+  }
+
+
 }
 
 export { Point, Wallet, Person, Customer, Vendor };
